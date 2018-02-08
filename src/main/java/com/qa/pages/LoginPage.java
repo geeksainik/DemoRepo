055798c751d0de7711bcf9de7c9c09486a5d016e
geeksainik <sairam9.0m@gmail.com>
2018-02-08 19:13:55 +0530

    Added Search Page

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,5 +1,13 @@
 package com.qa.pages;
 
 public class LoginPage {
+	public void loginpage()
+	{
+		System.out.println("this is login");
+	}
+	public void loginset()
+	{
+		System.out.println("this is set method");
+	}
 
 }
